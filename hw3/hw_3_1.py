976882129d866731324a948f96328a6e7a53036a
Ligh7Fun <m5221710@gmail.com>
2023-03-17 10:46:15 +0500

    fix #1

--- hw3/hw_3_1.py
+++ hw3/hw_3_1.py
@@ -1,10 +1,14 @@
 boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
 girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
-boys.sort()
-girls.sort()
+
 if len(boys) != len(girls):
     print('Количество мальчиков и девочек разное, '
           'кто-то может остаться без пары, знакомить не будем.')
 else:
+    boys.sort()
+    girls.sort()
+    # либо если длины списков равны, отдельно создать список
+    # pairs = list(zip(sorted(boys), sorted(girls)))
+    # и итерироваться по данному списку
     for unity_b, unity_g in zip(boys, girls):
         print(f'{unity_b} и {unity_g}')
