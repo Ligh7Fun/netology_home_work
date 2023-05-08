266cbcf02f3ad107c4e4f6871a15fa96edc85fda
Denis Tsybin <m5221710@gmail.com>
2023-05-08 14:04:58 +0500

    update

--- OOP/students_and_mentor.py
+++ OOP/students_and_mentor.py
@@ -1,3 +1,4 @@
+#  узнать как указать тип объеекта "класс" который описан ниже
 class Person:
     def __init__(self, name: str, surname: str):
         self.name = name
@@ -75,16 +76,17 @@ class Student(Person):
             self.finished_courses.append(course_name)
         print(f'Курс "{course_name}" добавлен в завершенные.')
 
-    def add_grade(self, course_name: str, grade: int):
+    def add_grade(self, lector, course_name: str, grade: int):
         if course_name in self.courses_in_progress and \
-                isinstance(grade, int) and 1 <= grade <= 10:
-            if course_name in self.grades:
-                self.grades[course_name].append(grade)
-            else:
-                self.grades[course_name] = [grade]
-            print(f'Для курса "{course_name}" добавлена оценка {grade}.')
+                isinstance(grade, int) and \
+                1 <= grade <= 10 and \
+                course_name in lector.courses_attached and \
+                isinstance(lector, Lecturer):
+            lector.grades.setdefault(course_name, []).append(grade)
+            print(f'Лектору {lector.surname} {lector.name} '
+                  f'для курса "{course_name}" добавлена оценка {grade}.')
         else:
-            print('Указали некорректную оценку или такого курса нет!\n')
+            print('Указали некорректные данные!\n')
 
     def __str__(self):
         if len(self.courses_in_progress):
@@ -108,16 +110,17 @@ class Mentor(Person):
         super().__init__(name, surname)
         self.courses_attached = []
 
-    def rate_hw(self, student: Student, course: str, grade: int):
+    def rate_hw(self, student, course: str, grade: int):
         if isinstance(student, Student) and \
                 course in self.courses_attached and \
                 course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
+            # if course in student.grades:
+            #     student.grades[course] += [grade]
+            # else:
+            #     student.grades[course] = [grade]
+            student.grades.setdefault(course, []).append(grade)
         else:
-            return 'Ошибка'
+            raise NotImplementedError('Данному классу запрещено выставлять оценки!')
 
     def __str__(self):
         return f'{super().__str__()}'
@@ -128,6 +131,10 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.courses_attached = []
         self.grades = {}
+
+    def rate_hw(self, student, course: str, grade: int):
+        raise NotImplementedError('Данному классу запрещено выставлять оценки!')
+
 
     def __eq__(self, other):
         if isinstance(other, Lecturer):
@@ -189,29 +196,44 @@ class Reviewer(Mentor):
 
 
 if __name__ == "__main__":
-    best_student = Student('Ruoy', 'Eman', 'your_gender')
-    best_student.add_course('Python')
-    best_student.add_course('Python2')
-    best_student.add_grade('Python', 5)
-    best_student.add_grade('Python', 7)
-    best_student.add_grade('Python2', 3)
-    best_student.add_finish_course('Python3')
-    print(best_student)
+    student1 = Student('Ivan', 'Petrov', 'M')
+    student2 = Student('Maria', 'Ivanova', 'F')
+
+    student1.add_course('Python')
+    student1.add_finish_course('C')
+    student2.add_course('C++')
+    student2.add_finish_course('Perl')
+
+    lector1 = Lecturer('Petr', 'Ivanov')
+    lector2 = Lecturer('Vlad', 'Smirnov')
+    lector1.courses_attached += ['Python']
+    lector2.courses_attached += ['C++']
+
+    reviewer1 = Reviewer('Artem', 'Lazarev')
+    reviewer2 = Reviewer('Damir', 'Petrov')
+    reviewer1.courses_attached += ['Python']
+    reviewer2.courses_attached += ['C++']
+    reviewer1.rate_hw(student1, 'Python', 5)
+    reviewer1.rate_hw(student1, 'Python', 7)
+    reviewer1.rate_hw(student1, 'Python', 10)
+    reviewer2.rate_hw(student2, 'C++', 4)
+    reviewer2.rate_hw(student2, 'C++', 6)
+    reviewer2.rate_hw(student2, 'C++', 9)
+
+    student1.add_grade(lector1, 'Python', 5)
+    student1.add_grade(lector1, 'Python', 9)
+    student1.add_grade(lector2, 'Python', 3)
+
+    student2.add_grade(lector1, 'C++', 4)
+    student2.add_grade(lector2, 'C++', 5)
+    student2.add_grade(lector2, 'C++', 7)
+
+    print(student1)
+    print(student2)
+    print(lector1)
+    print(lector2)
+    print(reviewer1)
+    print(reviewer2)
 
 
-    best_student2 = Student('Ruoy2', 'Eman2', 'your_gender')
-    best_student2.courses_in_progress += ['Python']
-    print(best_student2)
 
-    cool_mentor = Lecturer('Some', 'Buddy')
-    cool_mentor.courses_attached += ['Python']
-    print(cool_mentor)
-
-    # print(best_student == cool_mentor)
-
-    cool_mentor.rate_hw(best_student, 'Python', 10)
-    cool_mentor.rate_hw(best_student, 'Python', 10)
-    cool_mentor.rate_hw(best_student, 'Python', 10)
-
-    print(best_student.grades)
-    print(cool_mentor.courses_attached)
