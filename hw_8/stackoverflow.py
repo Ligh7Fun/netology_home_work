89de6c59b1bf20d2048d3b2e3f13b31c8f893428
Denis Tsybin <m5221710@gmail.com>
2023-05-17 18:08:57 +0500

    fix count questions

--- hw_8/stackoverflow.py
+++ hw_8/stackoverflow.py
@@ -5,14 +5,34 @@ import requests
 
 tags = 'Python'
 days = 2
+
+page = 1
+pagesize = 100
+has_more = True
+
+count_questions = 0
+
 now = datetime.datetime.today()
 to_date = int(time.mktime(datetime.date(now.year, now.month, now.day).timetuple()))
 from_date = int(time.mktime(datetime.date(now.year, now.month, now.day - days).timetuple()))
-url = f'https://api.stackexchange.com/2.3/questions?' \
-      f'fromdate={from_date}&todate={to_date}' \
-      f'&order=desc&sort=activity&tagged={tags}&site=stackoverflow'
-request = requests.get(url).json()
 
-for index, item in enumerate(request["items"], start=1):
-    print(f'====== Question {index}/{len(request["items"])} ======')
-    print(f'Author: {item["owner"]["display_name"]}\nTitle: {item["title"]}\nLink: {item["link"]}\n')
+while has_more:
+    url = f'https://api.stackexchange.com/2.3/questions?' \
+          f'fromdate={from_date}&todate={to_date}' \
+          f'&order=desc&sort=activity&tagged={tags}&site=stackoverflow' \
+          f'&pagesize={pagesize}&page={page}'
+
+    request = requests.get(url).json()
+
+    print(f'*** Page #{page} ***')
+
+    for index, item in enumerate(request["items"], start=1):
+        print(f'====== Question {index}/{len(request["items"])} ======')
+        print(f'Author: {item["owner"]["display_name"]}\nTitle: {item["title"]}\nLink: {item["link"]}\n')
+        count_questions += 1
+
+    has_more = request["has_more"]
+    page += 1
+
+print(f'Total pages: {page-1}')
+print(f'Total questions: {count_questions}')
